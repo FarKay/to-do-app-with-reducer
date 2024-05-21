b808a82dfe8a0b3b03d17d990ed35a855501719e
farouk <daudafarouk35@gmail.com>
2024-05-21 10:44:06 +0100

    added applayout component

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import List from "../components/List";
+import Todo from "./Todo";
+
+function AppLayout() {
+  return (
+    <div>
+      <Todo />
+      <List />
+    </div>
+  );
+}
+
+export default AppLayout;
